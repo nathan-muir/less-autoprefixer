94bd96a90769d8fb29acdacbaba57b55f86739bd
Firstname Lastname <your.email>
2015-03-21 10:53:22 +1100

    first commit

---
+++ package.js
@@ -0,0 +1,31 @@
+Package.describe({
+  name: 'flemay:less-autoprefixer',
+  version: '0.0.1',
+  summary: 'The dynamic stylesheet language + Autoprefixer',
+  git: 'https://github.com/flemay/less-autoprefixer',
+  documentation: 'README.md'
+});
+
+Package.registerBuildPlugin({
+  name: "compileLessAddAutoprefixer",
+  use: [],
+  sources: [
+    'lib/plugin/compile-less.js'
+  ],
+  npmDependencies: {
+    "less": "2.4.0",
+    "less-plugin-autoprefix": "1.4.0"
+  }
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.4.1');
+  api.addFiles('less-autoprefixer.js');
+});
+
+Package.onTest(function(api) {
+  api.use(['flemay:less-autoprefixer', 'test-helpers', 'tinytest', 'templating']);
+  api.add_files(['test/less_tests.less', 'test/less_tests.js', 'test/less_tests.html',
+    'test/less_tests_empty.less'
+  ], 'client');
+});
