903ac79433b682a38a11d47435dd8674cc2a4745
Firstname Lastname <your.email>
2015-03-21 14:56:15 +1100

    Summary is only 100 chars

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'flemay:less-autoprefixer',
   version: '1.0.0',
-  summary: 'This meteor package compiles your `.less` files into CSS, adds vendor prefixes to CSS rules and includes the results in the client CSS bundle.',
+  summary: 'The dynamic stylesheet language + Autoprefixer',
   git: 'https://github.com/flemay/less-autoprefixer',
   documentation: 'README.md'
 });
