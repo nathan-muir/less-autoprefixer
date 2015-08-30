52696f89e231308eb1819a6dbcb3fc33980d757b
Nathan Muir <ndmuir@gmail.com>
2015-08-30 22:49:24 +1000

    First build for METEOR@1.2-rc.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ var packageName = 'nathanmuir:less-autoprefixer';
 
 Package.describe({
   name: packageName,
-  version: '1.0.2-1',
+  version: '1.2.0-rc.0',
   summary: 'The dynamic stylesheet language + Autoprefixer',
   git: 'https://github.com/nathan-muir/less-autoprefixer',
   documentation: 'README.md'
@@ -10,14 +10,19 @@ Package.describe({
 
 Package.registerBuildPlugin({
   name: "compileLessAddAutoprefixer",
-  use: [],
+  use: ['caching-compiler', 'ecmascript', 'underscore'],
   sources: [
-    'lib/plugin/compile-less.js'
+    'plugin/compile-less.js'
   ],
   npmDependencies: {
-    "less": "2.5.1",
+    // Fork of 2.5.0, deleted large unused files in dist directory.
+    "less": "https://github.com/meteor/less.js/tarball/8130849eb3d7f0ecf0ca8d0af7c4207b0442e3f6",
     "less-plugin-autoprefix": "1.4.2"
   }
+});
+
+Package.onUse(function (api) {
+  api.use('isobuild:compiler-plugin@1.0.0');
 });
 
 Package.onTest(function(api) {
